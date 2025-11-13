228fcb30f6146079787d15db9928f0d0e77453b9
nothinaboutdany <decs20116@gmail.com>
2025-11-13 01:04:07 -0400

    Te amo dios

--- controlador.py
+++ controlador.py
@@ -28,14 +28,6 @@ def iniciar_juego():
     vista.dimensionar_pantalla_juego(modelo.ANCHO_PANTALLA, modelo.ALTO_PANTALLA, b"", 60)
     vista.cargar_texturas() 
 
-"""    def cerrar_ventana():
-    global corona
-    if corona:
-        raylib.UnloadTexture(corona)
-    raylib.CloseWindow()
-"""
-
-
 def jugar():
    # """Actualiza la lógica principal del juego."""
     modelo.movimiento_jugador()
@@ -110,8 +102,6 @@ def mostrar_menu_pausa():
         # Mostrar las opciones con resaltado
         vista.opciones_menu(modelo.OPCIONES_MENU_PAUSA, ancho, seleccion)
         vista.terminar_dibujo()
-
-
 
 #------------------------- MENÚ DE RESULTADOS ----------------------------
 
